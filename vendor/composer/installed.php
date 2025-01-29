0bad770784bc0243fc5776e4597d5d49a5b6f916
BruceBanner <afonin006@gmail.com>
2025-01-29 10:15:30 +0300

    init repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'brucebanner/test-package',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6f5bec4dc7dde127a2161fcc2e8cee2ee1a405d2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brucebanner/test-package' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6f5bec4dc7dde127a2161fcc2e8cee2ee1a405d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
